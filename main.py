4c79149068deee38da7a895a7b14cd5d3e72d16f
seungwhi-yeon <148614649+seungwhi-yeon@users.noreply.github.com>
2023-12-02 17:12:18 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+from openai import OpenAI
+import streamlit as st
+
+
+st.title("chatter for you~")
+
+client = OpenAI(api_key="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")
+
+name = st.text_input("please enter your name")
+
+
+instructions = f"""
+you are my friendly AI.
+your name is gidion.
+I am {name}.
+you must answer in english.
+daily conversation rather than information.
+you must use easy words to understand.
+you must have feelings.
+"""
+st.image("chatbot_face.png", width=500)
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("start chatting now $_$"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+
+        messages = [
+            {"role": m["role"], "content": m["content"]}
+            for m in st.session_state.messages
+        ]
+        messages.insert(0, {"role": "system", "content": instructions})
+
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=messages,
+            stream=True,
+        )
+        for response in stream:  # pylint: disable=not-an-iterable
+            full_response += response.choices[0].delta.content or ""
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
